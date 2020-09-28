cd02634b3a0cc2374bb1b20f66f906d22d65956c
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-28 06:27:41 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+  has_many :characters 
+  has_many :actors, through: :characters
+  belongs_to :newtwork
   
+  def actors_list 
+  self.actors.map {|a|"#{a.full_name}"}
+  end 
+   
+  # def build_network(netwrk)
+  #   self.network = Network.find_or_create_by(netwrk)
+  # end
 end
